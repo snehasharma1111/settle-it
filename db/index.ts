672920060952fdf150e2e1158de786d6e74c3cef
Akshat Mittal <akshatmittal2506@gmail.com>
2024-09-27 16:39:21 +0530

    fix: db connection timeout

--- db/index.ts
+++ db/index.ts
@@ -17,8 +17,13 @@ global.mongoose = global.mongoose || {
 };
 
 export class DatabaseManager {
+	private static isIntervalSet = false;
 	constructor() {
 		this.connect();
+		if (!DatabaseManager.isIntervalSet) {
+			DatabaseManager.isIntervalSet = true;
+			setInterval(() => this.ping(), 10000);
+		}
 	}
 
 	public async connect() {
@@ -29,7 +34,15 @@ export class DatabaseManager {
 
 		if (!global.mongoose.promise) {
 			global.mongoose.promise = mongoose
-				.connect(url.db)
+				.connect(url.db, {
+					connectTimeoutMS: 10000, // Timeout after 10 seconds if not connected
+					serverSelectionTimeoutMS: 5000, // Timeout for server selection
+					socketTimeoutMS: 45000, // Timeout for inactivity on the connection
+					// keepAlive: true, // Keep the connection alive
+					// keepAliveInitialDelay: 300000, // Send a keep-alive ping every 5 minutes})
+					autoIndex: true,
+					autoCreate: true,
+				})
 				.then((mongooseInstance) => {
 					logger.info("MongoDB connected");
 					return mongooseInstance;
@@ -63,9 +76,24 @@ export class DatabaseManager {
 		logger.info("MongoDB disconnected");
 	}
 
-	async ensureIndexes() {
+	private async ensureIndexes() {
 		await UserModel.createIndexes();
 		logger.info("MongoDB indexes created");
+	}
+
+	public async ping() {
+		if (!global.mongoose.conn) {
+			logger.info("MongoDB is not connected");
+			return false;
+		}
+		try {
+			await global.mongoose.conn.connection.db.command({ ping: 1 });
+			logger.info("MongoDB is connected");
+			return true;
+		} catch (error) {
+			logger.info("MongoDB is not connected");
+			return false;
+		}
 	}
 }
 
