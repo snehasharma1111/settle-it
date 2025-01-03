8976f054c19a2163c48d27c39a0a5e76a127d529
Sneha Sharma <snehasharma9205@gmail.com>
2025-01-04 02:40:18 +0530

    feat: added people cards for contributions summary

--- components/Groups/contributions.tsx
+++ components/Groups/contributions.tsx
@@ -1,9 +1,10 @@
+import { fallbackAssets } from "@/constants";
 import { useStore } from "@/hooks";
+import { Avatar, Typography } from "@/library";
 import { IShare } from "@/types";
 import { stylesConfig } from "@/utils";
 import React from "react";
 import styles from "./styles.module.scss";
-import { Typography } from "@/library";
 
 interface IGroupContributionsProps {
 	shares: Array<IShare>;
@@ -12,7 +13,7 @@ interface IGroupContributionsProps {
 const classes = stylesConfig(styles, "group-contributions");
 
 const GroupContributions: React.FC<IGroupContributionsProps> = ({ shares }) => {
-	const { accentColor } = useStore();
+	const { accentColor, theme } = useStore();
 	return (
 		<div className={classes("")}>
 			<div className={classes("-bar")}>
@@ -39,6 +40,49 @@ const GroupContributions: React.FC<IGroupContributionsProps> = ({ shares }) => {
 							>
 								{share.percentage}%
 							</Typography>
+						</div>
+					</div>
+				))}
+			</div>
+			<div className={classes("-cards")}>
+				{shares.map((record, recId) => (
+					<div
+						key={`owed-record-person-${recId}`}
+						className={classes("-person", "-person--block")}
+						style={{
+							backgroundColor: `rgba(${accentColor}, ${record.opacity})`,
+						}}
+					>
+						<div
+							className={classes("-person-details")}
+							style={{
+								color:
+									record.opacity > 0.5 || theme === "dark"
+										? "var(--theme-white)"
+										: "var(--theme-black)",
+							}}
+						>
+							<Avatar
+								src={
+									record.user.avatar || fallbackAssets.avatar
+								}
+								alt={record.user.name || record.user.email}
+								size={56}
+							/>
+							<div className={classes("-person-details__text")}>
+								<Typography size="lg">
+									{record.user.name || record.user.email}
+								</Typography>
+								<Typography size="s">
+									{record.percentage.toFixed(2)}%
+								</Typography>
+								<Typography size="s">
+									{new Intl.NumberFormat("en-US", {
+										style: "currency",
+										currency: "INR",
+									}).format(record.amount)}
+								</Typography>
+							</div>
 						</div>
 					</div>
 				))}
